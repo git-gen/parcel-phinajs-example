05cc11bb91204075575e49c4452030406fbe439e
Genki <gitkotobuki@gmail.com>
2021-01-16 23:45:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# parcel-phinajs-example
+
+parcelでphina.jsを動かすテンプレート
+
+## setup
+
+```bash
+# parcelをグローバルインストール
+$ yarn global add parcel-bundler
+
+$ cd parcel-phinajs-example
+$ yarn
+```
+
+## dev
+
+```bash
+$ yarn dev
+```
+
+## build
+
+```bash
+$ yarn build
+```
---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+import phina from 'phina.js'
+
+// phina.js をグローバル領域に展開
+phina.globalize();
+
+// MainScene クラスを定義
+phina.define('MainScene', {
+  superClass: 'DisplayScene',
+  init: function() {
+    this.superInit();
+    this.backgroundColor = '#444';
+    this.label = Label('Hello, phina.js!').addChildTo(this);
+    this.label.x = this.gridX.center();
+    this.label.y = this.gridY.center();
+    this.label.fill = 'white';
+  },
+});
+
+// メイン処理
+phina.main(function() {
+  // アプリケーション生成
+  var app = GameApp({
+    startLabel: 'main',
+  });
+  // アプリケーション実行
+  app.run();
+});
